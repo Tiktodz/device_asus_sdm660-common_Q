9e93f89b7ea3bd21b31c15ae6ccb840155a652d5
Tiktodz <ewprjkt@proton.me>
2022-05-21 12:14:26 +0000

    sdm660-common: Build sqlite3
    Change-Id: I80b59f31e1dcc8bb0e4adbfdb2e614d0000f29ef

--- sdm660.mk
+++ sdm660.mk
@@ -436,6 +436,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Sqlite
+PRODUCT_PACKAGES += \
+    sqlite3
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
